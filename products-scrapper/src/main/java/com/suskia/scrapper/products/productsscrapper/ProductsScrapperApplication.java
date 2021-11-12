2f0de2b03f53300ae13752b49c679dae697650c9
Adam Marian Suski <adamus.suski@gmail.com>
2021-11-12 14:56:12 +0100

    initial_commit
    initial commit for repo

---
+++ products-scrapper/src/main/java/com/suskia/scrapper/products/productsscrapper/ProductsScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.suskia.scrapper.products.productsscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsScrapperApplication.class, args);
+	}
+
+}
---
+++ products-scrapper/src/test/java/com/suskia/scrapper/products/productsscrapper/ProductsScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suskia.scrapper.products.productsscrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
